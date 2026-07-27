5f916a8ed04792f56ff339952e6915cf2c476e59
Jenna Brennan <jennab0608@gmail.com>
2026-07-27 16:26:16 -0400

    created dynamic calendar

--- StudyPlan.js
+++ StudyPlan.js
@@ -1,19 +1,46 @@
 const menuCloseButton = document.getElementById("menu-hide");
 const sidebar = document.getElementById("side-bar");
 const monthLeftButton = document.getElementById("nav-left");
+const calendarContainer = document.getElementById("calendar-container");
 const monthRightButton = document.getElementById("nav-right");
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const monthPlace = document.getElementById("monthPlace");
 const yearPlace = document.getElementById("yearPlace");
 const now = new Date();
+const daysContainer = document.getElementById("days");
 let monthNum = now.getMonth();
 let yearNum = now.getFullYear();
 monthPlace.textContent = months[monthNum];
 yearPlace.textContent = yearNum;
 
+function renderCalendar(){
+    daysContainer.innerHTML = "";
+    const firstDay = new Date(yearNum, monthNum, 1);
+    const lastDay = new Date(yearNum, monthNum + 1, 0);
+
+    const daysInMonth = lastDay.getDate();
+    const startingDay = firstDay.getDay();
+
+    for(let i = 0; i < startingDay; i++){
+        const emptyDay = document.createElement("div");
+        daysContainer.appendChild(emptyDay);
+    }
+
+    for(let day = 1; day <= daysInMonth; day++){
+        const dayBox = document.createElement("div");
+        dayBox.classList.add("day");
+        dayBox.textContent = day;
+        daysContainer.appendChild(dayBox);
+        if(day === now.getDate() && monthNum === now.getMonth() && yearNum === now.getFullYear()){
+            dayBox.classList.add("today");
+        }
+    }
+}
+
 
 menuCloseButton.addEventListener("click",()=> {
     sidebar.classList.toggle("hidden");
+    calendarContainer.classList.toggle("bigger");
 });
 
 monthRightButton.addEventListener("click",()=>{
@@ -25,6 +52,7 @@ monthRightButton.addEventListener("click",()=>{
         yearPlace.textContent = yearNum;
     }
     monthPlace.textContent = months[monthNum];
+    renderCalendar();
 });
 monthLeftButton.addEventListener("click", ()=>{
     if(monthNum > 0){
@@ -35,4 +63,6 @@ monthLeftButton.addEventListener("click", ()=>{
         yearPlace.textContent = yearNum;
     }
     monthPlace.textContent = months[monthNum];
+    renderCalendar();
 } )
+renderCalendar();
